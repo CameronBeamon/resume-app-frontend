aafb91333e9c4a865202c30a61cf29772051a811
Cameron Beamon <BeamonCameron@gmail.com>
2022-03-07 19:46:38 -0600

    Added login feature

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data: function () {
+    return {
+      Username: "",
+      Password: "",
+    };
+  },
+  methods: {
+    LogmeIn: function () {
+      console.log("hi");
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>Username:</p>
+    <input type="text" v-model="Username" />
+  </div>
+  <div>
+    <p>Password:</p>
+    <input type="text" v-model="Password" />
+  </div>
+  <button v-on:click="LogmeIn()">Login</button>
+</template>
