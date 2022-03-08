2e541caff78e7ef3239070bf63d5efee203b0a73
Kurt Proepper <nick.k.proepper@gmail.com>
2022-03-07 20:45:00 -0600

    Added Profile Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Profile from "../views/Profile.vue";
 
 const routes = [
   {
@@ -20,6 +21,12 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
+  },
+
+  {
+    path: "/profile",
+    name: Profile,
+    component: Profile,
   },
 ];
 
