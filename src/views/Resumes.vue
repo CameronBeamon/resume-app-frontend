c56b4f1ad8a0413acfa4fdc4345a136ab77083de
Cameron Beamon <BeamonCameron@gmail.com>
2022-03-09 19:34:35 -0600

    added other model attributes to the view all resumes page

--- src/views/Resumes.vue
+++ src/views/Resumes.vue
@@ -42,17 +42,29 @@ export default {
     <p><a href="resume.resume_url"></a></p>
     <p><a href="resume.github_url">GitHub</a></p>
     <img v-bind:src="resume.photo_url" alt="personal image" class="profile-photo" />
+    <h1>Experience</h1>
     <div v-for="experience in resume.experiences" :key="experience.id">
-      <h1>Experience</h1>
       <p>{{ experience.company_name }}</p>
     </div>
-
-    <!-- <h1>Education</h1>
-    <p>{{ educations }}</p>
+    <h1>Education</h1>
+    <div v-for="education in resume.educations" :key="education.id">
+      <p>{{ education.university_name }}</p>
+      <p>{{ education.degree }}</p>
+      <p>{{ education.details }}</p>
+      <p>{{ education.start_date }}</p>
+      <p>{{ education.end_date }}</p>
+    </div>
     <h1>Skills</h1>
-    <p>{{ skills }}</p>
+    <div v-for="skill in resume.skills" :key="skill.id">
+      <p>{{ skill.skill_name }}</p>
+    </div>
     <h1>Projects</h1>
-    <p>{{ projects }}</p> -->
+    <div v-for="project in resume.projects" :key="project.id">
+      <p>{{ project.project_name }}</p>
+      <p>{{ project.description }}</p>
+      <img :src="project.project_screenshot" alt="" />
+      <a href="project.project_url">Project Link</a>
+    </div>
     <p>__________________________</p>
   </div>
 </template>
