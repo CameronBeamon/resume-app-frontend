e62332571a628e5624aecc2067e7644de4e9cc49
Cameron Beamon <BeamonCameron@gmail.com>
2022-03-09 20:09:45 -0600

    fixed updated profile actions

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -15,15 +15,15 @@ export default {
     console.log("looks like it's working");
   },
   methods: {
-    updateProfile: function (profile) {
+    updateProfile: function () {
       axios
-        .patch("/students/" + profile.id, this.editProfileParams)
+        .patch("https://fierce-cove-12825.herokuapp.com/students/" + localStorage.student_id, this.editProfileParams)
         .then((response) => {
           console.log("profile update", response);
-          this.$router.push("/");
+          this.$router.push("/profile");
         })
         .catch((error) => {
-          console.log("products update error", error.response);
+          console.log("profile update error", error.response);
           this.errors = error.response.data.errors;
         });
     },
@@ -44,57 +44,55 @@ export default {
     <p>Personal Blog: {{ profile.personal_blog_url }}</p>
     <p>Github: {{ profile.github_url }}</p>
     <p>Twitter: {{ profile.twitter_handle }}</p>
-    <dialog id="photo-details">
-      <form method="dialog">
-        <h1>profile info</h1>
-        <p>
-          first Name:
-          <input type="text" v-model="editProfileParams.first_name" />
-        </p>
-        <p>
-          Last Name:
-          <input type="text" v-model="editProfileParams.last_name" />
-        </p>
-        <p>
-          Email:
-          <input type="text" v-model="editProfileParams.email" />
-        </p>
-        <p>
-          PhoneNumber:
-          <input type="text" v-model="editProfileParams.phone_number" />
-        </p>
-        <p>
-          Bio:
-          <input type="text" v-model="editProfileParams.short_bio" />
-        </p>
-        <p>
-          Linkedin:
-          <input type="text" v-model="editProfileParams.linkedin_url" />
-        </p>
-        <p>
-          Twitter:
-          <input type="text" v-model="editProfileParams.twitter_handle" />
-        </p>
-        <p>
-          Portfolio:
-          <input type="text" v-model="editProfileParams.personal_blog_url" />
-        </p>
-        <p>
-          Resume:
-          <input type="text" v-model="editProfileParams.resume_url" />
-        </p>
-        <p>
-          Github:
-          <input type="text" v-model="editProfileParams.github_url" />
-        </p>
-        <p>
-          Picture:
-          <input type="text" v-model="editProfileParams.photo_url" />
-        </p>
-        <button v-on:click="updateProfile(currentProfile)">Update</button>
-        <button v-on:click="updateProfile(currentProfile)">Update</button>
-        <button>Close</button>
-      </form>
-    </dialog>
+
+    <form method="dialog">
+      <h1>profile info</h1>
+      <p>
+        first Name:
+        <input type="text" v-model="editProfileParams.first_name" />
+      </p>
+      <p>
+        Last Name:
+        <input type="text" v-model="editProfileParams.last_name" />
+      </p>
+      <p>
+        Email:
+        <input type="text" v-model="editProfileParams.email" />
+      </p>
+      <p>
+        PhoneNumber:
+        <input type="text" v-model="editProfileParams.phone_number" />
+      </p>
+      <p>
+        Bio:
+        <input type="text" v-model="editProfileParams.short_bio" />
+      </p>
+      <p>
+        Linkedin:
+        <input type="text" v-model="editProfileParams.linkedin_url" />
+      </p>
+      <p>
+        Twitter:
+        <input type="text" v-model="editProfileParams.twitter_handle" />
+      </p>
+      <p>
+        Portfolio:
+        <input type="text" v-model="editProfileParams.personal_blog_url" />
+      </p>
+      <p>
+        Resume:
+        <input type="text" v-model="editProfileParams.resume_url" />
+      </p>
+      <p>
+        Github:
+        <input type="text" v-model="editProfileParams.github_url" />
+      </p>
+      <p>
+        Picture:
+        <input type="text" v-model="editProfileParams.photo_url" />
+      </p>
+      <button v-on:click="updateProfile(currentProfile)">Update</button>
+      <button>Close</button>
+    </form>
   </div>
 </template>
